500dacd2151df1d9186ffed153c29acc6dca43f4
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-12-24 13:33:56 -0500

    starting to work on v2 - no longer using replit - using v0.0.5 of kaboomjs (issues w/collision in v2000.1.2) - game not 100% back to what it was - no more circle() for snowball (rect() instead) - fixed unlimited jump bug - area() breaks everything in v0.0.5 - only using static contents (no node deps) - fixed enemy (bunny) collisions with trees - haven't focused on scenes or music yet

--- code/main.js
+++ code/main.js
@@ -1,11 +1,10 @@
-import kaboom from "kaboom";
-
 // initialize context
 kaboom(
   {
     width: 800,
     height: 500,
-    background: [ 179, 209, 249, ]
+    background: [179, 209, 249,],
+    global: true,
   }
 );
 
@@ -18,7 +17,7 @@ const MAGIC_SPEED = 400
 const SNOWBALL_SPEED = 200
 const ENEMY_SPEED = 40
 const FALL_DEATH = 600
-let LEVEL_INDEX = args.level ?? 0 
+let LEVEL_INDEX = args.level ?? 0
 let SCORE_GLOBAL = args.score ?? 0
 
 
@@ -48,7 +47,7 @@ loadSprite("arrowRight", "sprites/arrowRight.png");
 loadSprite("arrowLeft", "sprites/arrowLeft.png");
 loadSprite("buttonStart", "sprites/buttonStart.png");
 loadSprite("arrowUp", "sprites/arrowUp.png");
-loadPedit("magic", "sprites/magic.pedit");
+// loadPedit("magic", "sprites/magic.pedit");
 loadSprite("lightning-blue", "sprites/lightning-blue.png");
 loadSprite("environment", "sprites/environment.png");
 loadSprite("play-button", "sprites/play-button.png");
@@ -86,12 +85,12 @@ loadSound("shrink", "sounds/shrink.mp3");
 /* --------------- SCENES -----------------*/
 // TO-DO: win scene
 
-  // add layers
-  layers(['bg','obj', 'ui'], 'obj')
+// add layers
+layers(['bg', 'obj', 'ui'], 'obj')
 
-  // map creation
-  const maps = [
-    [
+// map creation
+const maps = [
+  [
     '                         =%            ',
     '                                       ',
     '                                       ',
@@ -108,7 +107,7 @@ loadSound("shrink", "sounds/shrink.mp3");
     '                   ^                   ',
     '                                       ',
     '===============================  ======',
-    ], [
+  ], [
     '                         =%            ',
     '                                       ',
     '                                       ',
@@ -125,8 +124,8 @@ loadSound("shrink", "sounds/shrink.mp3");
     '                         ^             ',
     '                                       ',
     '_______________________________  ______',
-    ],
-    [
+  ],
+  [
     '                                       ',
     '    w                                  ',
     '                                       ',
@@ -143,35 +142,35 @@ loadSound("shrink", "sounds/shrink.mp3");
     '                                       ',
     '                                       ',
     '-------------------------------  ------',
-    ],
-    ]
+  ],
+]
 
-  const levelCfg = {
-    width: 20,
-    height: 20,
-    '=': () => [sprite('block-4'), 'ground', 'block', solid(), scale(0.35), area()],
-    '$': () => [sprite('present'), 'green-present', 'gift', solid(), scale(0.9), area()],
-    'j': () => [sprite('candy-cane2'), 'candy-cane','gift', solid(), scale(0.35), area()],
-    'l': () => [sprite('lightning-blue'), 'lightning-blue', 'gift', solid(), scale(0.2), area()],
-    '%': () => [sprite('mystery-box2'), 'present-surprise','surprise-box', solid(), scale(0.35), area()],
-    '*': () => [sprite('mystery-box2'), 'candy-cane-surprise','surprise-box', solid(), scale(0.35), area()],
-    'z': () => [sprite('mystery-box2'), 'lightning-surprise','surprise-box', solid(), scale(0.35), area()],
-    '\}': () => [sprite('unboxed'), solid(), scale(0.35), area()],
-    '|': () => [sprite('lamp-post'),'post', area(), solid()],
-    '^': () => [sprite('bunny-enemy'), 'b-enemy', 'bleft', solid(), scale(0.2), area(), body()],
-    'b': () => [sprite('bunny-enemy'), 'b-enemy', 'bright', solid(), scale(0.2), area(), body()],
-    '-': () => [sprite('block-2'), 'ground', 'melting', solid(), scale(0.35), area()],
-    '_': () => [sprite('block-3'), 'ground', 'melting', solid(), scale(0.35), area()],
-    'x': () => [sprite('block-5'), 'ground', solid(), scale(0.35), area()],
-    't': () => [sprite('tree'), 'right-tree', solid(), scale(0.45), area()],
-    'f': () => [sprite('tree'), 'left-tree', solid(), scale(0.45), area()],
-    'w': () => [sprite('wingMan1'), 's-enemy', solid(), scale(0.3), area()],
-  }
+const levelCfg = {
+  width: 20,
+  height: 20,
+  '=': () => [sprite('block-4'), 'ground', 'block', solid(), scale(0.35)],
+  '$': () => [sprite('present'), 'green-present', 'gift', solid(), scale(0.9)],
+  'j': () => [sprite('candy-cane2'), 'candy-cane', 'gift', solid(), scale(0.35)],
+  'l': () => [sprite('lightning-blue'), 'lightning-blue', 'gift', solid(), scale(0.2)],
+  '%': () => [sprite('mystery-box2'), 'present-surprise', 'surprise-box', solid(), scale(0.35)],
+  '*': () => [sprite('mystery-box2'), 'candy-cane-surprise', 'surprise-box', solid(), scale(0.35)],
+  'z': () => [sprite('mystery-box2'), 'lightning-surprise', 'surprise-box', solid(), scale(0.35)],
+  '\}': () => [sprite('unboxed'), solid(), scale(0.35)],
+  '|': () => [sprite('lamp-post'), 'post', solid()],
+  '^': () => [sprite('bunny-enemy'), 'b-enemy', 'bleft', solid(), scale(0.2), body(), {dir: 1}],
+  'b': () => [sprite('bunny-enemy'), 'b-enemy', 'bright', solid(), scale(0.2), body(), {dir: 1}],
+  '-': () => [sprite('block-2'), 'ground', 'melting', solid(), scale(0.35)],
+  '_': () => [sprite('block-3'), 'ground', 'melting', solid(), scale(0.35)],
+  'x': () => [sprite('block-5'), 'ground', solid(), scale(0.35)],
+  't': () => [sprite('tree'), 'right-tree', 'tree', solid(), scale(0.45)],
+  'f': () => [sprite('tree'), 'left-tree', 'tree', solid(), scale(0.45)],
+  'w': () => [sprite('wingMan1'), 's-enemy', solid(), scale(0.3)],
+}
 
 let introMusic;
 // menu scene
 scene('menu', () => {
-  
+
   // intro music
   introMusic = play("introMusic", {
     volume: 0.3,
@@ -183,7 +182,7 @@ scene('menu', () => {
     sprite('instructions'),
     layer('bg'),
     origin('center'),
-    pos(width()/2, height()/2),
+    pos(width() / 2, height() / 2),
     scale(1)
   ])
 
@@ -198,9 +197,9 @@ scene('menu', () => {
     'play'
   ])
 
-  onClick('play', (p) => {
+  clicks('play', (p) => {
     play('mouseClick', {
-      volume:0.8,
+      volume: 0.8,
     })
     go('game')
   })
@@ -211,22 +210,22 @@ let loseMusic, winMusic;
 // game scene
 scene('game', () => {
 
-  introMusic.pause()
-  if (loseMusic) {
-    loseMusic.pause()
-  }
-  if (winMusic) {
-    winMusic.pause()
-  }
+  // introMusic.pause()
+  // if (loseMusic) {
+  //   loseMusic.pause()
+  // }
+  // if (winMusic) {
+  //   winMusic.pause()
+  // }
 
-  // gameplay music
-  gameplayMusic = play("gameplay", {
-      volume: 0.3,
-      loop: true
-  })
+  // // gameplay music
+  // gameplayMusic = play("gameplay", {
+  //   volume: 0.3,
+  //   loop: true
+  // })
 
   // add layers
-  layer(['obj', 'ui'], 'obj')
+  layer(['bg','obj', 'ui'], 'obj')
 
   // variables
   let CURRENT_JUMP_FORCE = JUMP_FORCE
@@ -239,8 +238,8 @@ scene('game', () => {
     sprite('environment'),
     layer('bg'),
     origin('topleft'),
-    scale(width()/240, height()/240),
-    // scale(1)
+    scale(width() / 240, height() / 240),
+    scale(1)
   ])
 
   const gameLevel = addLevel(maps[LEVEL_INDEX], levelCfg)
@@ -248,35 +247,35 @@ scene('game', () => {
   // add a score
   const score = add([
     text(SCORE_GLOBAL),
-    pos(20,6),
+    pos(20, 6),
     layer('ui'),
     {
-      value: SCORE_GLOBAL,  
-    }, 
-    scale(.4),
-    fixed()
+      value: SCORE_GLOBAL,
+    },
+    scale(2),
+    // fixed()
   ])
 
   // add level numbers
   const level = add([
-    text('level ' + parseInt(LEVEL_INDEX + 1)), 
-    pos(90,6), 
+    text('level ' + parseInt(LEVEL_INDEX + 1)),
+    pos(90, 6),
     {
       value: LEVEL_INDEX,
     },
-    scale(0.4),
-    fixed()
+    scale(2),
+    // fixed()
   ])
 
   // add player
-  const player = add([sprite('santa'), pos(50,0), area(), body(), big(), scale(.65)])
+  const player = add([sprite('santa'), pos(50, 0), body(), big(), scale(.65), origin('bot')])
 
   // add camera movement
-  player.onUpdate(() => {
+  player.action(() => {
     camPos(player.pos)
-    if(player.pos.y >= FALL_DEATH) {
+    if (player.pos.y >= FALL_DEATH) {
       play('fallOff', {
-      volume:0.8,
+        volume: 0.8,
       })
       go('lose', {
         score: SCORE_GLOBAL
@@ -293,16 +292,17 @@ scene('game', () => {
     player.move(MOVE_SPEED, 0)
   })
 
-  player.onUpdate(() => {
-    if (player.isGrounded()) {
+  player.action(() => {
+    if (player.grounded()) {
       isJumping = false
     }
   })
 
   keyPress('space', () => {
-    if(player.isGrounded())
+    if (player.grounded()) {
       isJumping = true
-    player.jump(CURRENT_JUMP_FORCE)
+      player.jump(CURRENT_JUMP_FORCE)
+    }
   })
 
   keyDown('up', () => {
@@ -311,7 +311,7 @@ scene('game', () => {
 
   keyDown('s', () => {
     play('shoot', {
-      volume:0.8,
+      volume: 0.8,
     })
     spawnSnowBall(player.pos.add(0, -35))
   })
@@ -320,19 +320,18 @@ scene('game', () => {
   // add magic casting
   function spawnMagic(p) {
     add([
-      rect(3,3),
+      rect(3, 3),
       pos(p),
       origin('center'),
-      area(),
       color(44, 171, 77),
       'magic'
     ])
   }
 
   // add magic motion
-  onUpdate('magic', (m) => {
+  action('magic', (m) => {
     m.move(0, -MAGIC_SPEED)
-    if (m.pos.y < 0){
+    if (m.pos.y < 0) {
       destroy(m)
     }
   })
@@ -340,46 +339,47 @@ scene('game', () => {
   // add snowball shooting
   function spawnSnowBall(p) {
     add([
-      circle(6),
+      rect(6,6),
       pos(p),
       origin('center'),
-      area(),
       color(255, 255, 255),
       'snowball'
     ])
   }
 
   // add snowball motion
-  onUpdate('snowball', (s) => {
+  action('snowball', (s) => {
     s.move(0, -SNOWBALL_SPEED)
-    if (s.pos.y < 0){
+    if (s.pos.y < 0) {
       destroy(s)
     }
   })
 
   // add bunny enemy motion
-  onUpdate('bleft', (b) => {
-    b.move(CURRENT_E_SPEED, 0)
+  action('bleft', (b) => {
+    b.move(CURRENT_E_SPEED * b.dir, 0)
   })
 
   // add bunny enemy motion
-  onUpdate('bright', (b) => {
-    b.move(-CURRENT_E_SPEED, 0)
+  action('bright', (b) => {
+    b.move(-CURRENT_E_SPEED * b.dir, 0)
   })
 
   // add sunbeam enemy motion
-  onUpdate('s-enemy', (s) => {
+  action('s-enemy', (s) => {
     s.move(0, CURRENT_S_SPEED)
-    if (s.pos.y < 0){
+    if (s.pos.y < 0) {
       destroy(s)
     }
   })
 
-  // TO-DO: fix enemy collides with left wall
-  onCollide('s-enemy', 'ground', (s, g) => {
-    destroy(g)
+  collides('b-enemy', 'tree', (b, t) => {
+    b.dir = -b.dir
   })
-  
+  collides('b-enemy', 'b-enemy', (b, t) => {
+    b.dir = -b.dir
+  })
+
 
   // make santa grow 
   function big() {
@@ -388,7 +388,7 @@ scene('game', () => {
     return {
       update() {
         if (isBig) {
-          timer -=dt()
+          timer -= dt()
           if (timer <= 0) {
             this.smallify()
           }
@@ -399,7 +399,7 @@ scene('game', () => {
       },
       smallify() {
         play('shrink', {
-          volume:0.8,
+          volume: 0.8,
         })
         this.scale = vec2(.65)
         timer = 0
@@ -408,115 +408,113 @@ scene('game', () => {
       },
       biggify(time) {
         play('grow', {
-          volume:0.5,
+          volume: 0.5,
         })
         this.scale = vec2(1)
         timer = time
         isBig = true
-              CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+        CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
       }
     }
   }
 
-  /* --------------- COLLISIONS -----------------*/
+  // /* --------------- COLLISIONS -----------------*/
 
   // -- magic collides with boxes -- 
   // surprise box - green present 
-  onCollide('magic', 'present-surprise', (m, p) => {
+  collides('magic', 'present-surprise', (m, p) => {
     play('giftReveal', {
-      volume:0.5,
+      volume: 0.5,
     })
-    gameLevel.spawn('$', p.gridPos.sub(1,2))
-    gameLevel.spawn('=', p.gridPos.sub(0,0))
+    gameLevel.spawn('$', p.gridPos.sub(1, 2))
+    gameLevel.spawn('=', p.gridPos.sub(0, 0))
     destroy(m)
   })
 
   // surprise box - candy cane
-  onCollide('magic', 'candy-cane-surprise', (m, c) => {
+  collides('magic', 'candy-cane-surprise', (m, c) => {
     play('giftReveal', {
-      volume:0.5,
+      volume: 0.5,
     })
-    gameLevel.spawn('j', c.gridPos.sub(0,1))
-    gameLevel.spawn('=', c.gridPos.sub(0,0))
+    gameLevel.spawn('j', c.gridPos.sub(0, 1))
+    gameLevel.spawn('=', c.gridPos.sub(0, 0))
     destroy(m)
   })
 
   // surprise box - blue lightning
-  onCollide('magic', 'lightning-surprise', (m, l) => {
+  collides('magic', 'lightning-surprise', (m, l) => {
     play('giftReveal', {
-      volume:0.5,
+      volume: 0.5,
     })
-    gameLevel.spawn('l', l.gridPos.sub(0,1.5))
-    gameLevel.spawn('=', l.gridPos.sub(0,0))
+    gameLevel.spawn('l', l.gridPos.sub(0, 1.5))
+    gameLevel.spawn('=', l.gridPos.sub(0, 0))
     destroy(m)
   })
-    
+
   // regular block
-  onCollide('magic', 'block', (m, b) => {
+  collides('magic', 'block', (m, b) => {
     destroy(m)
   })
 
-  // TO-DO: fix snowball area() bug 
-  // snowball restore melting snow box
-  // onCollide('snowball', 'melting', (s, m) => {
-  //   play('hitWithSnowBall', {
-  //     volume:0.5,
-  //   })
-  //   gameLevel.spawn('=', m.gridPos.sub(0,0))
-  //   SCORE_GLOBAL+=15
-  //   score.text = SCORE_GLOBAL
-  //   destroy(s)
-  // })
+  // // TO-DO: fix snowball area() bug 
+  // // snowball restore melting snow box
+  // // collides('snowball', 'melting', (s, m) => {
+  // //   play('hitWithSnowBall', {
+  // //     volume:0.5,
+  // //   })
+  // //   gameLevel.spawn('=', m.gridPos.sub(0,0))
+  // //   SCORE_GLOBAL+=15
+  // //   score.text = SCORE_GLOBAL
+  // //   destroy(s)
+  // // })
 
-  // -- enemies collide with objects --
-  // TO-DO: Change to box
-  // onCollide('s-enemy', 'snowball', (e, s) => {
-  //   play('hitWithSnowBall')
-  //   destroy(e)
-  //   destroy(s)
-  // })
+  // // -- enemies collide with objects --
+  // // TO-DO: Change to box
+  // // collides('s-enemy', 'snowball', (e, s) => {
+  // //   play('hitWithSnowBall')
+  // //   destroy(e)
+  // //   destroy(s)
+  // // })
 
   // -- player collides with objects --
   // blue lightning
-  player.onCollide('lightning-blue', (b) => {
+  player.collides('lightning-blue', (b) => {
     player.biggify(7)
     destroy(b)
   })
 
   // green present
-  player.onCollide('green-present', (g) => {
+  player.collides('green-present', (g) => {
     play('collectGift', {
-      volume:0.5,
+      volume: 0.5,
     })
     destroy(g)
-    SCORE_GLOBAL+=10
+    SCORE_GLOBAL += 10
     score.text = SCORE_GLOBAL
-    console.log(score)
   })
 
   // candy cane
-  player.onCollide('candy-cane', (c) => {
+  player.collides('candy-cane', (c) => {
     play('collectGift', {
-      volume:0.5,
+      volume: 0.5,
     })
     destroy(c)
-    SCORE_GLOBAL+=10
+    SCORE_GLOBAL += 10
     score.text = SCORE_GLOBAL
-    console.log(score)
   })
 
   // bunny enemies
-  player.onCollide('b-enemy', (b) => {
+  player.collides('b-enemy', (b) => {
     if (isJumping) {
       play('jumpOnEnemy', {
-      volume:0.9,
+        volume: 0.9,
       })
       destroy(b)
-      SCORE_GLOBAL+=5
-    score.text = SCORE_GLOBAL
+      SCORE_GLOBAL += 5
+      score.text = SCORE_GLOBAL
     } else {
       play('runIntoEnemy', {
-      volume:0.5,
+        volume: 0.5,
       })
       go('lose', {
         level: (LEVEL_INDEX),
@@ -526,17 +524,17 @@ scene('game', () => {
   })
 
   // sunbeam enemies
-  player.onCollide('s-enemy', (s) => {
+  player.collides('s-enemy', (s) => {
     if (isJumping) {
       play('jumpOnEnemy', {
-      volume:0.9,
+        volume: 0.9,
       })
       destroy(s)
-      SCORE_GLOBAL+=5
-    score.text = SCORE_GLOBAL
+      SCORE_GLOBAL += 5
+      score.text = SCORE_GLOBAL
     } else {
       play('runIntoEnemy', {
-      volume:0.5,
+        volume: 0.5,
       })
       go('lose', {
         level: (LEVEL_INDEX),
@@ -546,20 +544,20 @@ scene('game', () => {
   })
 
   // lamp post 
-  player.onCollide('post', (p) => {
+  player.collides('post', (p) => {
     LEVEL_INDEX++
-    SCORE_GLOBAL+=100
+    SCORE_GLOBAL += 100
     score.text = SCORE_GLOBAL
     gameplayMusic.pause()
-    if(LEVEL_INDEX > 2){
+    if (LEVEL_INDEX > 2) {
       go('win', {
         score: SCORE_GLOBAL
       })
     } else {
       play('levelUp', {
-        volume:0.5,
+        volume: 0.5,
       })
-        go('game', {
+      go('game', {
         level: level.value,
         score: SCORE_GLOBAL
       })
@@ -570,47 +568,47 @@ scene('game', () => {
 // lose scene  
 scene('lose', () => {
 
-  gameplayMusic.pause()
+  // gameplayMusic.pause()
 
   // lose scene music
   loseMusic = play("loseScene", {
-      volume: 0.3,
-      loop: true
+    volume: 0.3,
+    loop: true
   })
 
   add([
     sprite('lose-scene'),
     layer('bg'),
     origin('center'),
-    pos(width()/2, height()/2),
+    pos(width() / 2, height() / 2),
     scale(1)
   ])
 
   add([
-    text('Score: ' + SCORE_GLOBAL), 
+    text('Score: ' + SCORE_GLOBAL),
     origin('center'),
     pos(388, 292),
-    scale(.6)
+    scale(5)
   ])
 
   add([
     sprite('play-button'),
     layer('bg'),
     origin('center'),
-    pos(width()/2, 395),
+    pos(width() / 2, 395),
     scale(.07),
     area(),
     'play'
   ])
-  onClick('play', (p) => {
+  clicks('play', (p) => {
     loseMusic.pause()
     play('mouseClick', {
-      volume:0.8,
+      volume: 0.8,
     })
     LEVEL_INDEX = 0
     SCORE_GLOBAL = 0
     go('game')
-    })
+  })
 })
 
 // win scene
@@ -620,38 +618,38 @@ scene('win', () => {
 
   // win scene music
   winMusic = play("winScene", {
-      volume: 0.3,
-      loop: true
+    volume: 0.3,
+    loop: true
   })
 
   add([
     sprite('win-scene'),
     layer('bg'),
     origin('center'),
-    pos(width()/2, height()/2),
+    pos(width() / 2, height() / 2),
     scale(1)
   ])
-  
+
   add([
-  text('Score: ' + SCORE_GLOBAL), 
-  origin('center'),
-  pos(388, 292),
-  scale(.6)
+    text('Score: ' + SCORE_GLOBAL),
+    origin('center'),
+    pos(388, 292),
+    scale(.6)
   ])
 
   add([
     sprite('play-button'),
     layer('bg'),
     origin('center'),
-    pos(width()/2, 395),
+    pos(width() / 2, 395),
     scale(.07),
     area(),
     'play'
   ])
-  onClick('play', (p) => {
+  clicks('play', (p) => {
     winMusic.pause()
     play('mouseClick', {
-      volume:0.8,
+      volume: 0.8,
     })
     LEVEL_INDEX = 0
     SCORE_GLOBAL = 0
@@ -659,4 +657,4 @@ scene('win', () => {
   })
 })
 
-go('menu')
+start('game')
